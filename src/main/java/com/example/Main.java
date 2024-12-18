b9e13a3499a9b493e04d3eb99afefcf73bbabf54
kamalesh <kamaleshsathiyan18@gmail.com>
2024-12-18 12:08:25 +0530

    jenkins-maven-file

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! This is a Java application running from Jenkins pipeline.");
+    }
+}
+
